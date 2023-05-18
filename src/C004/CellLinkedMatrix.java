b1743dd4c4a6e34c4e27a6e14985cea8d5f5d00a
40321491 <bmacgillivray01@qub.ac.uk>
2023-05-18 22:23:26 +0100

    CellLinkedMatrix addition

--- src/C004/CellLinkedMatrix.java
+++ src/C004/CellLinkedMatrix.java
@@ -1,5 +1,7 @@
 package C004;
 
+import java.util.ArrayList;
+
 public class CellLinkedMatrix {
 
     int colSize;
@@ -20,30 +22,44 @@ public class CellLinkedMatrix {
             cellList[i] = new Cell(i);
         }
 
-        // link up all the cells
-        for (int i = 1; i <= cellAmount; i++) {
-            int tempCellID = cellList[i].getCellID();
-            // N
-            int tempVal = tempCellID - rowSize - 1;
-            if (boundCheck(tempVal, colSize, rowSize)) {
+        // identify the corners
+        int NW = 1;
+        int NE = col;
+        int SE = col * row;
+        int SW = (col * row) - col + 1;
 
-            }
-            // NE
-            // E
-            // SE
-            // S
-            // SW
-            // W
-            // NW
+        // identify the north border
+        int[] N = new int[col];
+        for (int i = 1; i < col; i++) {
+            N[i] = i;
         }
+        // remove the corners
+        N[0] = -1;
+        N[col-1] = -1;
 
-    }
+        // identify the east border
+        int[] E = new int[row];
+        E[0] = col;
+        for (int i = 1; i < row; i++) {
+            E[i] = E[i-1] + col;
+        }
+        // remove the corners
+        E[0] = -1;
+        E[row-1] = -1;
 
-    private boolean boundCheck(int tempVal, int col, int row) {
-        if ( tempVal > 0 || tempVal < row || tempVal < col) {
-            return true;
-        } else {
-            return false;
+        // identify the south border
+        int[] S = new int[col];
+        S[0] = ((col*row)-col+1);
+        for (int i = 1; i < col; i++) {
+            S[i] = S[i-1] + 1;
+        }
+        // remove corners
+        S[0] = -1;
+        S[col-1] = -1;
+
+
+        for (int i = 0; i < row; i++) {
+            System.out.println(S[i]);
         }
     }
 
--- src/C004/Main.java
+++ src/C004/Main.java
@@ -2,8 +2,8 @@ package C004;
 
 public class Main {
     public static void main(String[] args) {
-        int colSize = 10;
-        int rowSize = 10;
+        int colSize = 3;
+        int rowSize = 3;
         CellLinkedMatrix matrix = new CellLinkedMatrix(colSize, rowSize);
         // test
     }
